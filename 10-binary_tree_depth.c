1afff23a4f3b3525f668c8a768b90e86d454a038
MinutePapillon <christophebarrere@gmail.com>
2025-04-07 17:07:54 +0200

    fix: add condition tree == NULL c:13

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t height = 0;
 
-	if (tree->parent == NULL)
+	if (tree->parent == NULL || tree == NULL)
 		return (0);
 	while (tree->parent != NULL)
 	{
