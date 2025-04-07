a6f72591f6503746786425d765f774306c2139d0
N4yth <nathan.dupuis31@gmail.com>
2025-04-07 13:47:42 +0200

    style: remove white space L14 and add documentation

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_delete - function that free the binary tree
  *
+ * @tree: the root of the tree
  *
- *
- *
+ * Return: nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return ;
+		return;
 
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
